4ea75b40fbdab18e217ff6d6c41605fcb8cd92b5
suuedaerturk06-lang <suuedaerturk06@gmail.com>
2026-01-02 21:11:23 +0300

    Update web_service_app.py

--- web_service/web_service_app.py
+++ web_service/web_service_app.py
@@ -12,15 +12,4 @@ def index():
     if request.method == "POST":
         try:
             isim = request.form.get("isim")
-            sehir = request.form.get("sehir")
-
-            r = requests.post(
-                API_URL,
-                json={"isim": isim, "sehir": sehir},
-                timeout=5
-            )
-
-            if r.status_code == 200:
-                sonuc = r.json().get("mesaj", "Mesaj alınamadı")
-            else:
-                sonuc = "API servis hata verdi"
+            sehir = request.form
