8988e210da02b22fd3d95118c9055be12c8c4110
suuedaerturk06-lang <suuedaerturk06@gmail.com>
2026-01-02 19:03:53 +0300

    Update web_service.py

--- web_service/web_service.py
+++ web_service/web_service.py
@@ -1,10 +1,3 @@
-from flask import Flask, render_template_string, request, redirect
-import requests
-
-app = Flask(__name__)
-
-API_URL = "https://hello-cloud-3-1.onrender.com"
-
 HTML = """
 <!doctype html>
 <html>
@@ -15,4 +8,26 @@ body { font-family: Arial; text-align: center; padding: 50px; background: #eef2f
 h1 { color: #333; }
 input { padding: 10px; font-size: 16px; }
 button { padding: 10px 15px; background: #4CAF50; color: white; border: none; border-radius: 6px; cursor: pointer; }
-l
+li { background: white; margin: 5px auto; width: 200px; padding: 8px; border-radius: 5px; }
+</style>
+</head>
+
+<body>
+<h1>Mikro Hizmetli Selam!</h1>
+
+<p>Adını yaz</p>
+<form method="POST">
+<input type="text" name="isim" placeholder="Adını yaz" required>
+<button type="submit">Gönder</button>
+</form>
+
+<h3>Ziyaretçiler:</h3>
+<ul>
+{% for ad in isimler %}
+<li>{{ ad }}</li>
+{% endfor %}
+</ul>
+
+</body>
+</html>
+"""
