30acef43635444beb143a925cef55354bd722b18
suuedaerturk06-lang <suuedaerturk06@gmail.com>
2026-01-02 19:18:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
-app = Flask (__name__)
+
+app = Flask(__name__)
 
 @app.route('/')
-def home() :
+def home():
     return "Merhaba, Buluttan Selam!"
-  
+
+# 🔴 ÇOK ÖNEMLİ
+application = app
